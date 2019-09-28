963aeb8a48bfd9b04052c1af47246055cb6dcf12
Jerekian <proskur.egor@yandex.ru>
2019-09-28 21:29:09 +0300

    Final version. Decorative edits

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -16,6 +17,7 @@ public class PyramidBuilder {
         // TODO : Implement your solution here
 
         int n = isPossibleBuildPyramid(inputNumbers.size());
+
         if (inputNumbers.size() != n*(n+1)/2) throw new CannotBuildPyramidException();
 
         return buildPyramid(inputNumbers, n);
@@ -24,21 +26,27 @@ public class PyramidBuilder {
     private int[][] buildPyramid(List<Integer> inputNumbers, int n){
         int[][] pyramid = new int[n][2*n-1];
         if(inputNumbers.contains(null)) throw new CannotBuildPyramidException();
-        inputNumbers.sort((o1, o2) -> o1-o2);
+        inputNumbers.sort(Comparator.comparingInt(o -> o));
 
-        int numbersOnLine = 1, numberIndex = 0;
-        for(int i = 0; i < pyramid.length; i++){
+        int numberIndex = 0;
+        for(int i = 0, numbersOnLine = 1; i < pyramid.length; i++, numbersOnLine++){
             for(int j = n-i-1, count = 0; count < numbersOnLine ; count++, j+=2){
                 Integer number = inputNumbers.get(numberIndex);
                 numberIndex++;
                 pyramid[i][j] = number;
             }
-            numbersOnLine++;
         }
         return pyramid;
     }
 
     private int isPossibleBuildPyramid(int size){
+        /*
+        size = n*(n+1)/2;
+        n^2 + n - 2*size = 0;
+        D = 1^2 - 4*1*(-2*size);
+        n1 = sqrt(-1 + (1 + 8*size))/2
+        n2 < 0;
+         */
         return (int)(Math.sqrt(1+8*size) - 1)/2;
     }
 
